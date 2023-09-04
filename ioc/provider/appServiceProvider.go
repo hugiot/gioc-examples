c10076032c579c0481f298d567a3479f36da0e9f
xiaocai <1643013940@qq.com>
2023-09-04 11:06:23 +0800

    upgrade gioc v0.2.0

--- ioc/provider/appServiceProvider.go
+++ ioc/provider/appServiceProvider.go
@@ -1,26 +1,26 @@
 package provider
 
 import (
+	"github.com/hugiot/gioc"
 	"github.com/hugiot/gioc-examples/config"
 	"github.com/hugiot/gioc-examples/ioc/service"
 	"github.com/hugiot/gioc-examples/logger"
-	"github.com/hugiot/gioc/src/interfaces"
 	"github.com/spf13/viper"
 )
 
 type AppServiceProvider struct {
 }
 
-func (asp *AppServiceProvider) Register(c interfaces.ServiceContainer) {
-	c.Single(service.Config, func(sc interfaces.ServiceContainer) any {
+func (asp *AppServiceProvider) Register(c gioc.ServiceContainer) {
+	c.Single(service.Config, func(sc gioc.ServiceContainer) any {
 		return config.New()
 	})
-	c.Single(service.Logger, func(sc interfaces.ServiceContainer) any {
+	c.Single(service.Logger, func(sc gioc.ServiceContainer) any {
 		c := sc.Make(service.Config).(*viper.Viper)
 		return logger.New(c)
 	})
 }
 
-func (asp *AppServiceProvider) Boot(c interfaces.ServiceContainer) {
+func (asp *AppServiceProvider) Boot(c gioc.ServiceContainer) {
 
 }
--- main.go
+++ main.go
@@ -1,17 +1,17 @@
 package main
 
 import (
+	"github.com/hugiot/gioc"
 	"github.com/hugiot/gioc-examples/ioc/provider"
 	"github.com/hugiot/gioc-examples/ioc/service"
-	"github.com/hugiot/gioc/src/container"
 	"go.uber.org/zap"
 )
 
 func main() {
-	container.AddServerProvider(&provider.AppServiceProvider{})
-	container.Boot()
+	gioc.AddServerProvider(&provider.AppServiceProvider{})
+	gioc.Boot()
 
-	logger := container.Make(service.Logger).(*zap.Logger)
+	logger := gioc.Make(service.Logger).(*zap.Logger)
 	defer func() {
 		_ = logger.Sync()
 	}()
